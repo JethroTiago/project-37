769253f927afb33f2bc6091a6734ec6ee81af050
Jethro <jethrotiago@hotmail.com>
2023-02-04 20:09:59 -0300

    Manipulating the DOM

--- assets/app/raffleNumbers.js
+++ assets/app/raffleNumbers.js
@@ -1,7 +1,16 @@
+const lowerValue = 1;
+const highestValue = 100;
+
 const secretNumber = generateRandomNumber();
 
 function generateRandomNumber() {
-    return parseInt(Math.random() * 100);
+    return parseInt(Math.random() * highestValue + 1);
 }
 
 console.log('The secret number is:', secretNumber);
+
+const lowestValueElement = document.getElementById('lowest-value');
+lowestValueElement.innerHTML = lowerValue;
+
+const highestValueElement = document.getElementById('highest-value');
+highestValueElement.innerHTML = highestValue;
