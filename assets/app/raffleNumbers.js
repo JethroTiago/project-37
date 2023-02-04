4719b32bb459dbcc2fa3077c24b4816257a64926
Jethro <jethrotiago@hotmail.com>
2023-02-04 11:21:51 -0300

    Generating random number

--- assets/app/raffleNumbers.js
+++ assets/app/raffleNumbers.js
@@ -1,1 +1,7 @@
-console.log('Working in progress...');
+const secretNumber = generateRandomNumber();
+
+function generateRandomNumber() {
+    return parseInt(Math.random() * 100);
+}
+
+console.log('The secret number is:', secretNumber);
