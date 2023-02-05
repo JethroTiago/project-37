61d40000815667d9b3fae460d4ced9c6cd4b9df0
Jethro <jethrotiago@hotmail.com>
2023-02-04 21:42:21 -0300

    Creating first validations

--- assets/app/validity.js
+++ assets/app/validity.js
@@ -1,7 +1,20 @@
 function checksGuessValidity(chute) {
     const number = +guess;
 
-    if (Number.isNaN(number)) {
+    if (invalidGuess(number)) {
         console.log('Invalid value');
     }
+
+    if (greaterOrLessGuess(number)) {
+        console.log(`Invalid value. The secret number is between ${lowerValue} and ${highestValue}`);
+    }
+
+}
+
+function invalidGuess(number) {
+    return Number.isNaN(number);
+}
+
+function greaterOrLessGuess(number) {
+    return number > highestValue || number < lowerValue
 }
